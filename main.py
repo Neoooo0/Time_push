28fb7a2c69fa369e18dd9662e92c0cf56f358668
Neoooo0 <111836996+Neoooo0@users.noreply.github.com>
2022-08-27 14:47:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_access_token():
     app_id = os.environ["appid"]
     #config["app_id"]
     # appSecret
-    app_secret = config["app_secret"]
+    app_secret = os.environ["APP_SECRET"]
     post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
                 .format(app_id, app_secret))
     try:
